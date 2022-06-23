97e819066c92ec52463dcc443c822e48352962dd
TomoyaNishi <nishi.tomoya.519@gmail.com>
2022-06-24 00:40:31 +0900

    スタイル調整

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { PostFetch } from "./PostFetch";
 function App() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [disabled, setDisabled] = useState(true);
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -15,7 +16,16 @@ function App() {
     await PostFetch("http://localhost:8080/todos", input);
     GetFetch("http://localhost:8080/todos", setTodos);
     setInput("");
+    setDisabled(true);
   };
+
+  useEffect(() => {
+    if (input.length !== 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  });
 
   useEffect(() => {
     GetFetch("http://localhost:8080/todos", setTodos);
@@ -23,14 +33,27 @@ function App() {
 
   return (
     <div className="container">
-      <input value={input} onChange={(e) => handleChange(e)} />
-      <button onClick={() => handleClick()}>button</button>
-      <ul className="">
+      <div className="input-wrap">
+        <input
+          className="input-form"
+          value={input}
+          onChange={(e) => handleChange(e)}
+        />
+        <button
+          className={disabled ? "is-disabled" : "add-button"}
+          disabled={disabled}
+          onClick={() => handleClick()}
+        >
+          ADD
+        </button>
+      </div>
+      <ul className="lists">
         {todos
           ? todos.map((todo, index) => {
               return (
                 <li key={index} className="list-item">
-                  ({todo.id}) {todo.text}
+                  <p>{todo.text}</p>
+                  <button className="delete-button">DELETE</button>
                 </li>
               );
             })
